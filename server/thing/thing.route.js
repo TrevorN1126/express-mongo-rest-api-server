438acf4e4a4c8b43991c7b7d32e58d585b461184
TrevorN1126 <trevorn1126@gmail.com>
2020-02-05 01:27:52 -0500

    Changed controllers into classes

--- server/thing/thing.route.js
+++ server/thing/thing.route.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const validate = require('express-validation');
 const paramValidation = require('../../config/param-validation');
-const thingCtrl = require('./thing.controller');
+const ThingController = require('./thing.controller');
 const protectRoute = require('../middleware/protectRoute');
 
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+const ThingControllerInstance = new ThingController();
+
 router.route('/')
   /** GET /api/things - Get list of things */
-  .get(thingCtrl.list)
+  .get(ThingControllerInstance.list)
 
   /** POST /api/things - Create new thing */
-  .post(protectRoute([['user'], ['admin']]), validate(paramValidation.createThing), thingCtrl.create);
+  .post(protectRoute([['user'], ['admin']]), validate(paramValidation.createThing), ThingControllerInstance.create);
 
 router.route('/:thingId')
   /** GET /api/things/:thingId - Get thing */
-  .get(thingCtrl.get)
+  .get(ThingControllerInstance.get)
 
   /** PUT /api/things/:thingId - Update thing */
-  .put(protectRoute(['admin']), thingCtrl.update)
+  .put(protectRoute(['admin']), ThingControllerInstance.update)
 
   /** DELETE /api/things/:thingId - Delete thing */
-  .delete(protectRoute(['admin']), thingCtrl.remove);
+  .delete(protectRoute(['admin']), ThingControllerInstance.remove);
 
 module.exports = router;
--- server/user/user.route.js
+++ server/user/user.route.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const validate = require('express-validation');
 const paramValidation = require('../../config/param-validation');
-const userCtrl = require('./user.controller');
+const UserController = require('./user.controller');
 const protectRoute = require('../middleware/protectRoute');
 
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+const UserControllerInstance = new UserController();
+
 router.route('/')
   /** GET /api/users - Get list of users */
-  .get(protectRoute(['admin']), userCtrl.list)
+  .get(protectRoute(['admin']), UserControllerInstance.list)
 
   /** POST /api/users - Create new user */
-  .post(protectRoute(['admin']), validate(paramValidation.createUser), userCtrl.create);
+  .post(protectRoute(['admin']), validate(paramValidation.createUser), UserControllerInstance.create);
 
 router.route('/:userId')
   /** GET /api/users/:userId - Get user */
-  .get(protectRoute(['admin']), userCtrl.get)
+  .get(protectRoute(['admin']), UserControllerInstance.get)
 
   /** PUT /api/users/:userId - Update user */
-  .put(protectRoute(['admin']), validate(paramValidation.updateUser), userCtrl.update)
+  .put(protectRoute(['admin']), validate(paramValidation.updateUser), UserControllerInstance.update)
 
   /** DELETE /api/users/:userId - Delete user */
-  .delete(protectRoute(['admin']), userCtrl.remove);
+  .delete(protectRoute(['admin']), UserControllerInstance.remove);
 
 module.exports = router;
